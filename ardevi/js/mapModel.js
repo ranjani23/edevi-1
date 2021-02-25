3ef1cd54ddda0f5b5dedfabf08a8a40b6d2f9d75
Gudipati <52106126@BLRCLM5210612.local>
2021-02-25 19:08:36 +0530

    AR_Threads

---
+++ ardevi/js/mapModel.js
@@ -0,0 +1,29 @@
+var models = {
+    "rod": {
+        id: "#rod",
+        position: "0 -5 -20",
+        rotation:"0 0 0",
+        scale: "2 2 2"
+    },
+    "coin": {
+        id: "#coin",
+        position: "0 0 -10",
+        rotation:"0 180 0",
+        scale: "0.01 0.01 0.01"
+    },
+    "flower": {
+        id: "#flower",
+        position: "0 -3 -10",
+        rotation:"0 0 0",
+        scale: "2.5 2.5 2.5"
+    },
+    "snow_flake": {
+        id: "#snow_flake",
+        position: "0 -1 -3",
+        rotation:"0 0 0",
+        scale: "4 4 4",
+        animation: true,
+        attribute: "rotation"
+    }
+
+}

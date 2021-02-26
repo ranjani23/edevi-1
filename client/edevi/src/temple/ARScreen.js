48d48694ff634e42289d3a640196f9142367acbf
Gudipati <52106126@BLRCLM5210612.local>
2021-02-26 19:37:30 +0530

    iframe

--- client/edevi/src/temple/ARScreen.js
+++ client/edevi/src/temple/ARScreen.js
@@ -4,7 +4,7 @@ function ARScreen (props) {
     //TODO: Wire in the AR Iframe asset
     return (
         <div className='ARScreen'>
-            <iframe className='ARIframe'/>
+            <iframe src="https://glib-bouncy-minute.glitch.me/"  width="350px" height="600px" allow="camera" className='ARIframe'/>    
         </div>
     )
 }
